79e95cf37d5fe40dfa2beff0da8e8499caaa6a43
13935 <hl13935@ly.com>
2016-03-25 20:02:01 +0800

    新增redis

---
+++ Redis/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using CommonLibrary;
+
+
+namespace Redis
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            RedisHelper.CreateClient("127.0.0.1", 6379);
+            RedisHelper.SetString("test", "test2", 1000);
+
+            People Cold = new People();
+            Cold.Name = "测试";
+            Cold.age = 22;
+
+            RedisHelper.Set<People>("testT", Cold, 1000);
+
+            var tt=RedisHelper.Get<People>("testT");
+
+            Console.WriteLine(RedisHelper.GetStringValue("test"));
+            Console.ReadKey();
+        }
+
+        
+    }
+
+    class People
+    {
+        public string Name;
+        public int age;
+    }
+}
