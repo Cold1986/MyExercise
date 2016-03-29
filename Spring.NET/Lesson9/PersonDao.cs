836a97da98a430b6a5efd120fe4d359bc96ec424
13935 <hl13935@ly.com>
2016-03-29 16:59:28 +0800

    Spring.NET学习笔记9——打造简易的依赖注入框架(练习篇) Level 100

---
+++ Spring.NET/Lesson9/PersonDao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson9
+{
+    public class PersonDao
+    {
+        private int intProp;
+
+        public PersonDao(int intProp)
+        {
+            this.intProp = intProp;
+        }
+
+        public Person Entity { get; set; }
+
+        public override string ToString()
+        {
+            return "构造函数参数intProp为：" + this.intProp;
+        }
+    }
+}
