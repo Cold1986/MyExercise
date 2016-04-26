6533bb4159afabe26c6c7c2fcd5af7430633361b
13935 <hl13935@ly.com>
2016-04-26 15:51:27 +0800

    新增页面验证帮助类

---
+++ CommonLibrary/Path/WebSitePathHelper.cs
@@ -0,0 +1,77 @@
+﻿using System.Web;
+namespace CommonLibrary
+{
+    /// <summary>
+    /// 网站路径操作类
+    /// </summary>
+    public class WebSitePathHelper
+    {
+       
+        /// <summary>
+        /// 根据给出的相对地址获取网站绝对地址
+        /// </summary>
+        /// <param name="localPath">相对地址</param>
+        /// <returns>绝对地址</returns>
+        public static string GetWebPath(string localPath)
+        {
+            string path = HttpContext.Current.Request.ApplicationPath;
+            string thisPath;
+            string thisLocalPath;
+            //如果不是根目录就加上"/" 根目录自己会加"/"
+            if (path != "/")
+            {
+                thisPath = path + "/";
+            }
+            else
+            {
+                thisPath = path;
+            }
+            if (localPath.StartsWith("~/"))
+            {
+                thisLocalPath = localPath.Substring(2);
+            }
+            else
+            {
+                return localPath;
+            }
+            return thisPath + thisLocalPath;
+        }
+
+        /// <summary>
+        ///  获取网站绝对地址
+        /// </summary>
+        /// <returns></returns>
+        public static string GetWebPath()
+        {
+            string path = System.Web.HttpContext.Current.Request.ApplicationPath;
+            string thisPath;
+            //如果不是根目录就加上"/" 根目录自己会加"/"
+            if (path != "/")
+            {
+                thisPath = path + "/";
+            }
+            else
+            {
+                thisPath = path;
+            }
+            return thisPath;
+        }
+
+        /// <summary>
+        /// 根据相对路径或绝对路径获取绝对路径
+        /// </summary>
+        /// <param name="localPath">相对路径或绝对路径</param>
+        /// <returns>绝对路径</returns>
+        public static string GetFilePath(string localPath)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(localPath, @"([A-Za-z]):\\([\S]*)"))
+            {
+                return localPath;
+            }
+            else
+            {
+                return System.Web.HttpContext.Current.Server.MapPath(localPath);
+            }
+        }
+    }
+}
