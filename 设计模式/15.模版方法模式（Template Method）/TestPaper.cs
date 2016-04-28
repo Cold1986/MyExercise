06df51a4f24af1ca988e60beab44e16802d7205b
cold1986 <735255078@qq.com>
2016-04-28 22:56:35 +0800

    新增设计模式  模板方法模式

---
+++ 设计模式/15.模版方法模式（Template Method）/TestPaper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 模版方法模式
+{
+    class TestPaper
+    {
+        public void TestQuestion1()
+        {
+            Console.WriteLine("杨过得到，后来给了郭靖，练成倚天剑，屠龙刀的玄铁可能是【】a.球磨铸铁 b.马口铁 c.高速合金钢 d.碳素纤维");
+            Console.WriteLine("答案： " + Answer1());
+        }
+
+        public void TestQuestion2()
+        {
+            Console.WriteLine("杨过，程英，陆无双铲除了情花，造成【】a.使这种植物不再害人 b.使一种珍惜物种灭绝 c.破坏了那个生物圈的生态平衡 d.造成了该地区沙漠化");
+            Console.WriteLine("答案： " + Answer2());
+        }
+
+        public void TestQuestion3()
+        {
+            Console.WriteLine("蓝凤凰致使华山师徒，桃谷六仙呕吐不止，如果你是大夫，会给他们开什么药【】a.阿司匹林 b.牛黄解毒片 c.让他们喝大量的牛奶 d.以上全不对");
+            Console.WriteLine("答案： " + Answer3());
+        }
+
+        protected virtual string Answer1()
+        {
+            return "";
+        }
+
+        protected virtual string Answer2()
+        {
+            return "";
+        }
+
+        protected virtual string Answer3()
+        {
+            return "";
+        }
+    }
+}
---
+++ 设计模式/15.模版方法模式（Template Method）/TestPaperA.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 模版方法模式
+{
+    class TestPaperA : TestPaper
+    {
+        protected override string Answer1()
+        {
+            return "b";
+        }
+
+        protected override string Answer2()
+        {
+            return "c";
+        }
+
+        protected override string Answer3()
+        {
+            return "a";
+        }
+    }
+}
